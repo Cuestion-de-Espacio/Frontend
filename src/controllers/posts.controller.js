bf8e05c4bf6cb8b4d912d6ec59ba1fa2cceb5888
Fabian-Martinez1 <fabianmartinezrincon.123@gmail.com>
2022-10-02 16:15:31 -0300

    Json parceado a html

--- src/controllers/posts.controller.js
+++ src/controllers/posts.controller.js
@@ -1,13 +1,19 @@
 import view from '../view/posts.html';
 
-export default async () => {
+export default async (idMounstro) => {
   const divElement = document.createElement("div");
   divElement.innerHTML = view;
+  const postsElement = divElement.querySelector("#posts");
 
   fetch('https://cartas-backend.herokuapp.com')
   .then((response) => response.json())
-  .then((data) => console.log(data));
+  .then((data) =>   
+    postsElement.innerHTML += `
+    <h3>${data[idMounstro].nombre}</h3>
+    <h3>${data[idMounstro].beneficios}</h3>
+    <h3>${data[idMounstro].description}</h3>
+    `
+  );
 ;
-
   return divElement;
 };
